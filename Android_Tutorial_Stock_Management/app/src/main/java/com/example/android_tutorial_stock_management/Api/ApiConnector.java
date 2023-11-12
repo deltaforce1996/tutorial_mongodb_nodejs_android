e208b0b7bd388d88f4eec863acb01da4ab6e6d89
Alif <deltaforce19436@gmail.com>
2023-11-12 12:28:11 +0700

    finish tutorial, but android need add viewmodel

--- Android_Tutorial_Stock_Management/app/src/main/java/com/example/android_tutorial_stock_management/Api/ApiConnector.java
+++ Android_Tutorial_Stock_Management/app/src/main/java/com/example/android_tutorial_stock_management/Api/ApiConnector.java
@@ -1,4 +1,5 @@
 package com.example.android_tutorial_stock_management.Api;
 
 public class InternetApi {
+
 }
---
+++ Android_Tutorial_Stock_Management/app/src/main/java/com/example/android_tutorial_stock_management/Repositoris/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.android_tutorial_stock_management.Repositoris;
+
+import com.example.android_tutorial_stock_management.Services.IProduct;
+
+public class ProductRepos {
+    private IProduct iProduct;
+    public ProductRepos(IProduct iProduct) {
+        this.iProduct = iProduct;
+    }
+}
